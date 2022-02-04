cd4565fb4c349c95de5e34002758e40212debb96
Bart Thate <bthate67@gmail.com>
2022-02-04 17:42:50 +0100

    use __dir__

--- gcid/all.py
+++ gcid/all.py
@@ -4,6 +4,34 @@
 "OTP-CR-117/19"
 
 
+def __dir__():
+    return (
+        "cfg",
+        "cmd",
+        "dbs",
+        "evt",
+        "flt",
+        "fnc",
+        "fnd",
+        "irc",
+        "jsn",
+        "krn",
+        "log",
+        "opt",
+        "prs",
+        "req",
+        "rss",
+        "rpt",
+        "tbl",
+        "tdo",
+        "thr",
+        "tmr",
+        "udp",
+        "usr",
+        "wsd"
+    )
+
+
 from gcid.tbl import Tbl
 
 
@@ -17,49 +45,25 @@ from gcid import jsn
 from gcid import krn
 from gcid import opt
 from gcid import prs
-from gcid import req
 from gcid import rpt
-from gcid import sta
 from gcid import tbl
 from gcid import thr
 from gcid import tmr
-from gcid import wsd
-
-
-Tbl.add(cfg)
-Tbl.add(cmd)
-Tbl.add(dbs)
-Tbl.add(evt)
-Tbl.add(flt)
-Tbl.add(fnc)
-Tbl.add(jsn)
-Tbl.add(krn)
-Tbl.add(opt)
-Tbl.add(prs)
-Tbl.add(req)
-Tbl.add(rpt)
-Tbl.add(sta)
-Tbl.add(tbl)
-Tbl.add(thr)
-Tbl.add(tmr)
-
 
 from gcid import fnd
 from gcid import irc
 from gcid import log
 from gcid import opt
+from gcid import req
 from gcid import rss
+from gcid import sta
 from gcid import tdo
 from gcid import udp
 from gcid import usr
+from gcid import wsd
 
 
-Tbl.add(fnd)
-Tbl.add(irc)
-Tbl.add(log)
-Tbl.add(opt)
-Tbl.add(req)
-Tbl.add(rss)
-Tbl.add(tdo)
-Tbl.add(udp)
-Tbl.add(usr)
+for mn in __dir__():
+    md = getattr(locals(), mn, None)
+    if md:
+        Tbl.add(md)
